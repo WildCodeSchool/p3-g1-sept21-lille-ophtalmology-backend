491a1afd8e3dcc644af9a103817f71eb17a6fb9b
KeerhemFR <keerhem.guillaume@hotmail.fr>
2022-01-18 14:10:22 +0100

    Added the contents route

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const { backPort } = require('./conf');
 const commentsRoutes = require('./routes/comments');
+const contentsRoutes = require('./routes/contents');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/comments', commentsRoutes);
+app.use('/contents', contentsRoutes);
 
 app.use('/', (req, res) => {
   res.status(404).send(`Route not found: ${req.method} ${req.url} `);
