53d4c21ff37484f5a08e665c3b118974810d3db6
KeerhemFR <keerhem.guillaume@hotmail.fr>
2022-01-18 17:14:56 +0100

    Added a videos routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { backPort } = require('./conf');
 const commentsRoutes = require('./routes/comments');
 const contentsRoutes = require('./routes/contents');
+const videosRoutes = require('./routes/videos');
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/comments', commentsRoutes);
 app.use('/contents', contentsRoutes);
+app.use('/videos', videosRoutes);
 
 app.use('/', (req, res) => {
   res.status(404).send(`Route not found: ${req.method} ${req.url} `);
